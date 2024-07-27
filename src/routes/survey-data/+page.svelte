cae805f834286a368b245864f588aaf8858f2bbd
Javierko <33197433+Javierko@users.noreply.github.com>
2024-07-27 17:09:38 +0200

    feat: user data table for downloads

---
+++ src/routes/survey-data/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import DataTable from './data-table.svelte';
+</script>
+
+<div class="flex h-screen w-full items-center justify-center">
+	<div class="flex w-full max-w-3xl items-center justify-center">
+		<DataTable />
+	</div>
+</div>
