bd9ecb103955be7729b9372282d3e58fd8a434a4
Javierko <33197433+Javierko@users.noreply.github.com>
2024-10-17 15:31:53 +0200

    feat: larger radio-group items

--- src/lib/shadcn/ui/radio-group/radio-group-item.svelte
+++ src/lib/shadcn/ui/radio-group/radio-group-item.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-import { RadioGroup as RadioGroupPrimitive } from "bits-ui";
-import Circle from "lucide-svelte/icons/circle";
-import { cn } from "$lib/utils.js";
-import { fade } from "svelte/transition";
+	import { RadioGroup as RadioGroupPrimitive } from 'bits-ui';
+	import Circle from 'lucide-svelte/icons/circle';
+	import { cn } from '$lib/utils.js';
+	import { fade } from 'svelte/transition';
 
-type $$Props = RadioGroupPrimitive.ItemProps;
-type $$Events = RadioGroupPrimitive.ItemEvents;
+	type $$Props = RadioGroupPrimitive.ItemProps;
+	type $$Events = RadioGroupPrimitive.ItemEvents;
 
-let className: $$Props["class"] = undefined;
-export let value: $$Props["value"];
-export { className as class };
+	let className: $$Props['class'] = undefined;
+	export let value: $$Props['value'];
+	export { className as class };
 </script>
 
 <RadioGroupPrimitive.Item
 	{value}
 	class={cn(
-		"border-primary/80 text-primary ring-offset-background focus-visible:ring-ring aspect-square h-5 w-5 rounded-full border focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+		'aspect-square h-6 w-6 rounded-full border border-primary/80 text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
 		className
 	)}
 	{...$$restProps}
@@ -23,8 +23,8 @@ export { className as class };
 >
 	<div class="flex items-center justify-center">
 		<RadioGroupPrimitive.ItemIndicator>
-			<div transition:fade={{duration: 200}}>
-				<Circle class="h-2.5 w-2.5 fill-current text-current" />
+			<div transition:fade={{ duration: 200 }}>
+				<Circle class="h-3 w-3 fill-current text-current" />
 			</div>
 		</RadioGroupPrimitive.ItemIndicator>
 	</div>
