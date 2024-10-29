b8b073a08c26a1c733fb8a121bdfa0c5ba5f0d91
Javierko <33197433+Javierko@users.noreply.github.com>
2024-10-29 10:51:46 +0100

    feat: svelte sonner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import { Toaster } from '$lib/shadcn/ui/sonner';
 </script>
+
+<Toaster position="top-right" richColors duration={10000} />
 
 <div class="flex h-screen w-full items-center justify-center">
 	<slot></slot>
