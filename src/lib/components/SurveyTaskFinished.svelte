e33213d28b7aa4175e14d6d4d12f18065e9e4258
Javierko <javierkoo21@gmail.com>
2025-11-14 21:37:46 +0100

    feat: finished slide text

--- src/lib/components/SurveyTaskFinished.svelte
+++ src/lib/components/SurveyTaskFinished.svelte
@@ -6,7 +6,16 @@
 <section class="card">
 	<Alert.Root variant="success">
 		<Icon icon="clarity:success-standard-line" class="h-4 w-4 text-green-500" />
-		<Alert.Title>Hotovo!</Alert.Title>
-		<Alert.Description>Děkujeme za účast v dotazníku.</Alert.Description>
+		<Alert.Title>Děkujeme za Váš čas a cenné odpovědi!</Alert.Title>
+		<Alert.Description>
+			<p>
+				Váš příspěvek pro Psychologický ústav AV ČR je klíčový pro náš výzkum a všechny Vaše
+				odpovědi zůstávají zcela anonymní.
+			</p>
+
+			<p>Tímto je dotazník úspěšně dokončen. Nyní ho můžete uzavřít.</p>
+
+			<p>V případě dotazů kontaktujte: Mgr. Davida Lacka, PhD. (lacko@psu.cas.cz).</p>
+		</Alert.Description>
 	</Alert.Root>
 </section>
